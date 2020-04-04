de557837b0eccaeb2f49aee586b7e0696fc4b9c1
Ronald van der Kooij <ronald@sharpsolutions.nl>
2020-04-04 12:50:10 +0200

    fixed2

---
+++ docs/precache-manifest.fb15910041ac1e91b9e0bf2a521d3d2d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2056f1e37b21dfe34fd7c47eaccca0ff",
+    "url": "/berenjacht/index.html"
+  },
+  {
+    "revision": "8982b07c08d4a04c857f",
+    "url": "/berenjacht/static/css/main.f29fa5a1.chunk.css"
+  },
+  {
+    "revision": "d68d290e38b8c229860b",
+    "url": "/berenjacht/static/js/2.2e220ccf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/berenjacht/static/js/2.2e220ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8982b07c08d4a04c857f",
+    "url": "/berenjacht/static/js/main.949dfe65.chunk.js"
+  },
+  {
+    "revision": "0d67d1fd7b218d39cf88",
+    "url": "/berenjacht/static/js/runtime-main.7db5befe.js"
+  },
+  {
+    "revision": "9443618a3719739e02b4df5345e5873d",
+    "url": "/berenjacht/static/media/backbutton.9443618a.png"
+  },
+  {
+    "revision": "0ba542022510c75625ddf61919188cb1",
+    "url": "/berenjacht/static/media/background.0ba54202.jpg"
+  },
+  {
+    "revision": "94e1879737510e19ad00f18fba0f663e",
+    "url": "/berenjacht/static/media/bear_icon.94e18797.png"
+  },
+  {
+    "revision": "12f81e1fbeae1d16b8c1c36653626d3e",
+    "url": "/berenjacht/static/media/bear_icon_brown.12f81e1f.png"
+  },
+  {
+    "revision": "d4d857fa051c1e79bfd7abf361036617",
+    "url": "/berenjacht/static/media/level_icon.d4d857fa.png"
+  },
+  {
+    "revision": "be73be38d4522511c3709033778b281a",
+    "url": "/berenjacht/static/media/level_icon_disabled.be73be38.png"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8d1775144e4d69ad8ed6927f7ebdc9b9.js"
+  "/berenjacht/precache-manifest.fb15910041ac1e91b9e0bf2a521d3d2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/berenjacht/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
